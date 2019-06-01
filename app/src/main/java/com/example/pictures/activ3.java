900e52aec5700bf9bcf1edc4743a64e64633dd4a
Кокер <ForceSoldier@yandex.ru>
2019-06-01 15:00:42 +0400

    test commit

--- app/src/main/java/com/example/pictures/activ3.java
+++ app/src/main/java/com/example/pictures/activ3.java
@@ -1,11 +1,9 @@
 package com.example.pictures;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class activ3 extends AppCompatActivity {
